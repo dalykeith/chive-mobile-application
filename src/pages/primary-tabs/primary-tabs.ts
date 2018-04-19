c5c71d3abd1fc0beea38b966e662bda0797086df
Matthew Shiel <mattshiel@gmail.com>
2018-04-19 05:14:17 +0100

    Improved front end structure and added tabs

---
+++ src/pages/primary-tabs/primary-tabs.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+/**
+ * Generated class for the PrimaryTabsPage tabs.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-primary-tabs',
+  templateUrl: 'primary-tabs.html'
+})
+export class PrimaryTabsPage {
+
+  homeRoot = 'HomePage'
+  favouritesRoot = 'FavouritesPage'
+  shoppingListRoot = 'ShoppingListPage'
+  aboutRoot = 'AboutPage'
+
+
+  constructor(public navCtrl: NavController) {}
+
+}
